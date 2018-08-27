4832243ea8c294229335ba46946670234852bf6a
zcdtk <zcdtk@github.com>
2018-08-27 10:30:39 +0800

    update

---
+++ src/app/ibizsys/components/ibiz-component.ts
@@ -0,0 +1,143 @@
+import { Input, OnInit } from '@angular/core';
+
+/**
+ * 组件基本类
+ *
+ * @export
+ * @class IBizComponent
+ */
+export class IBizComponent implements OnInit {
+
+    /**
+     * 编辑器宽高样式
+     *
+     * @type {*}
+     * @memberof IBizComponent
+     */
+    public styleCss: any = { width: '100%', height: '100%' };
+
+    /**
+     * 编辑器id
+     *
+     * @type {string}
+     * @memberof IBizComponent
+     */
+    @Input() id: string;
+
+    /**
+     * 编辑器名称
+     *
+     * @type {string}
+     * @memberof IBizComponent
+     */
+    @Input() name: string;
+
+    /**
+     * 表单部件对象
+     *
+     * @type {*}
+     * @memberof IBizComponent
+     */
+    @Input() form: any;
+
+    /**
+     * 编辑器高度
+     *
+     * @type {string}
+     * @memberof IBizComponent
+     */
+    @Input() height: string;
+
+    /**
+     * 编辑器宽度
+     *
+     * @type {string}
+     * @memberof IBizComponent
+     */
+    @Input() width: string;
+
+    /**
+     * 编辑器是否启用
+     *
+     * @type {*}
+     * @memberof IBizComponent
+     */
+    @Input() disabled: any
+
+    /**
+     * 编辑器提示信息
+     *
+     * @type {string}
+     * @memberof IBizComponent
+     */
+    @Input() placeholder: string;
+
+    /**
+     * 编辑器值项名称
+     *
+     * @type {string}
+     * @memberof IBizComponent
+     */
+    @Input() valueItem: string;
+
+    /**
+     * 表格部件对象，行编辑使用
+     *
+     * @type {*}
+     * @memberof IBizComponent
+     */
+    @Input() grid: any;
+
+    /**
+     * 表格行数据，行编辑使用
+     *
+     * @type {*}
+     * @memberof IBizComponent
+     */
+    @Input() data: any;
+
+    /**
+     * 编辑器值文本值
+     *
+     * @type {string}
+     * @memberof IBizComponent
+     */
+    @Input()
+    set value(val: any) {
+        this.setComponentValue(val);
+    }
+
+    /**
+     * 组件值
+     *
+     * @type {string}
+     * @memberof IBizComponent
+     */
+    public $value: string;
+
+    constructor() { }
+
+    ngOnInit(): void {
+        // 初始化宽高
+        if (Object.is(this.width, '100%')) {
+            Object.assign(this.styleCss, { width: this.width });
+        } else if (this.width && parseInt(this.width) > 0) {
+            Object.assign(this.styleCss, { width: `${this.width}px` });
+        }
+        if (Object.is(this.height, '100%')) {
+            Object.assign(this.styleCss, { height: this.height });
+        } else if (this.height && parseInt(this.height) > 0) {
+            Object.assign(this.styleCss, { height: `${this.height}px` });
+        }
+    }
+
+    /**
+     * 组件值设置
+     *
+     * @param {*} val
+     * @memberof IBizComponent
+     */
+    public setComponentValue(val: any) {
+        this.$value = val;
+    }
+}
--- src/app/ibizsys/components/ibiz-components.module.ts
+++ src/app/ibizsys/components/ibiz-components.module.ts
@@ -5,13 +5,13 @@ import { SharedModule } from '@shared/shared.module';
 import { IBizFormComponent } from './ibiz-form/ibiz-form.component';
 import { IBizFormGroupComponent } from './ibiz-form-group/ibiz-form-group.component';
 import { IBizFormItemComponent } from './ibiz-form-item/ibiz-form-item.component';
-import { IbizDatepickerComponent } from './ibiz-datepicker/ibiz-datepicker.component';
+import { IBizDatepickerComponent } from './ibiz-datepicker/ibiz-datepicker.component';
 
 const conponents = [
   IBizFormComponent,
   IBizFormGroupComponent,
   IBizFormItemComponent,
-  IbizDatepickerComponent
+  IBizDatepickerComponent
 ];
 
 @NgModule({
