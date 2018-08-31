ad04693913791bcb693ab3454e169b9b3268aca4
zcdtk <zcdtk@github.com>
2018-08-31 09:46:45 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { IBizEnvironment } from '@env/IBizEnvironment';
+import { Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
+import { IBizApp } from 'ibizsys';
 
 @Component({
-  selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+    selector: 'app-root',
+    template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'angularapp';
+export class AppComponent implements OnInit {
+    // title = 'angularapp';
+
+    private separator = ' - ';
+    constructor(private router: Router, private title: Title, private ibizapp: IBizApp) {
+    }
+
+    public ngOnInit(): void {
+        this.router.events.pipe(filter(evt => evt instanceof NavigationEnd)).subscribe((evt) => {
+            const routerData = this.ibizapp.getActivatedRouteDatas(this.ibizapp.$activatedRouteDatas.length - 1);
+            this.title.setTitle(`${routerData.title}${this.separator}${IBizEnvironment.BaseUrl}`);
+        });
+    }
 }
